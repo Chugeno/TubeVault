51a55d11930d94528a274ec153306a44dfcc1518
Chugeno <eugenioazurmendi@gmail.com>
2024-12-03 09:26:26 -0400

    update folders

---
+++ scripts/config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+    youtubeApiKey: 'TU_API_KEY_DE_YOUTUBE',
+    channelId: 'TU_ID_DE_CANAL',
+    tmdbApiKey: 'TU_API_KEY_DE_TMDB',
+    updateInterval: 3600000,
+    unlistedVideos: [
+        "VIDEO_ID_1",
+        "VIDEO_ID_2"
+    ]
+}; 
